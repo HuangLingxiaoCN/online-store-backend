08d176a19105a50a46ec420bfd763580b97bbacf
Lingxiao Huang <jasonhlx1213@gmail.com>
2021-12-20 00:02:42 +0200

    deploy

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,7 @@ mongoose
   .then(() => {
     app.listen(app.get('port'), () => {
       console.log(
-        '  App is running at http://localhost:%d in %s mode',
+        '  App is running in %s mode',
         app.get('port'),
         app.get('env')
       )
